d84e0aa4d29c50af620dc9ce1ae4f3c0371e5cbb
Duvvelsheyss <ivajlosem@gmail.com>
2019-01-21 12:30:31 +0200

    Second commit which contains if cases, removing the need to execute separate file one by one

---
+++ ifcases.py
@@ -0,0 +1,8 @@
+num = float(input("Enter server type: "))
+if num >= 0:
+    if num == 0:
+        print("c01.tmdcloud.com  / c01.tmdcloud.net / c01.tmdcloud.asia / c02.tmdcloud.com / c02.tmdcloud.net / c02.tmdcloud.asia / c03.tmdcloud.com c03.tmdcloud.net c03.tmdcloud.asia")
+    else:
+        print("s210.tmd.cloud / s310.tmd.cloud / s410.tmd.cloud s510.tmd.cloud / s910.tmd.cloud / s981.tmd.cloud")
+else:
+    print("r116.tmd.cloud / r117.tmd.cloud / r118.tmd.cloud")
